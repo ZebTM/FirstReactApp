7e0b77b86541dc7efecb45eb87847bd8fa0c9be7
Zachary Brown <zacharybrown@tinyhomeconsultingllc.com>
2021-08-10 16:45:32 -0400

    Added first button

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-if(module.hot) {
-    module.hot.accept();
-}
+
 // Create a react components
 const App = function() {
-    return <div>Hi there!</div>
-}
+    return (
+    <div> 
+        <label class = "label" for ="name">
+            Enter name:
+        </label>
+        <input id="name" type = "text" />
+        <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+    </div>
+    );
+};
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'));
